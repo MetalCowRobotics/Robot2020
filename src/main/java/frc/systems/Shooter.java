35dd0adf5cf10c10ab95dbbc82e1cdf22031a86a
metalcow-student <46615373+metalcow-student@users.noreply.github.com>
2020-01-18 16:17:52 -0500

    set up shooter

--- src/main/java/frc/systems/Shooter.java
+++ src/main/java/frc/systems/Shooter.java
@@ -1,17 +1,38 @@
 package frc.systems;
 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.lib14.MCR_SRX;
+import frc.lib14.XboxControllerMetalCow;
 import frc.robot.RobotMap;
 
 public class Shooter {
-    
-    private static MCR_SRX topShooter = new MCR_SRX(RobotMap.Shooter.TOP_MOTOR);
-    private static MCR_SRX bottomShooter = new MCR_SRX(RobotMap.Shooter.BOTTOM_MOTOR);
-    private static final SpeedControllerGroup SHOOTER = new SpeedControllerGroup(topShooter, bottomShooter); 
-    private static final Shooter instance = new Shooter();
+    private static final XboxControllerMetalCow driver = new XboxControllerMetalCow(0);
+    private CANSparkMax neo1;
+    private CANSparkMax neo2;
+    // private static MCR_SRX topShooter = new MCR_SRX(RobotMap.Shooter.TOP_MOTOR);
+    // private static MCR_SRX bottomShooter = new
+    // MCR_SRX(RobotMap.Shooter.BOTTOM_MOTOR);
+    private static SpeedControllerGroup shooter;
+    private static final Shooter instance = new Shooter(); // TODO: What does this do?
 
     private Shooter() {
+        neo1 = new CANSparkMax(0, MotorType.kBrushless);
+        neo2 = new CANSparkMax(1, MotorType.kBrushless);
+        // neo3 = new CANSparkMax(2, MotorType.kBrushless);
+        // neo4 = new CANSparkMax(3, MotorType.kBrushless);
+
+        shooter = new SpeedControllerGroup(neo1, neo2);
+
+        // neoTwo.follow(neoOne);
+        // newOne.getEncoder
+        // newTwo.clone()
+        // neoOne.set(speed);
+        // SpeedControllerGroup shooterR = new SpeedControllerGroup(neoOne, neoTwo);
+        // shooterL.follow(shooterR);
 
     }
 
@@ -20,14 +41,17 @@ public class Shooter {
     }
 
     public void runShooter() {
-        
+        double speed = driver.getLT();
+        shooter.set(speed);
+
     }
 
     public void stopShooter() {
-
+        shooter.set(0);
     }
 
     public void checkSpeed() {
-        
+        SmartDashboard.putNumber("Shooter Speed", neo1.getEncoder().getVelocity());// RPM
+
     }
 }
