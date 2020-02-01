28fb787d46d7e961f32e81d1c5714d9cc9277476
metalcow-student <46615373+metalcow-student@users.noreply.github.com>
2020-01-31 20:35:32 -0600

    added color matching

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,8 +7,10 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.I2C;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.util.Color;
 import frc.autonomous.ShootAndGo;
 import frc.lib14.MCRCommand;
 import frc.lib14.XboxControllerMetalCow;
@@ -16,7 +18,10 @@ import frc.systems.Climber;
 import frc.systems.DriveTrain;
 import frc.systems.Intake;
 import frc.systems.Shooter;
-
+import com.revrobotics.ColorMatch;
+import com.revrobotics.ColorMatchResult;
+import com.revrobotics.ColorSensorV3;
+import edu.wpi.first.wpilibj.I2C.Port;
 /**
  * The VM is configured to automatically run this class. If you change the name
  * of this class or the package after creating this project, you must also
@@ -24,12 +29,12 @@ import frc.systems.Shooter;
  */
 
 public class Robot extends TimedRobot {
-  DriveTrain drive = DriveTrain.getInstance();
-  Intake intake = Intake.getInstance();
-  Shooter shooter = Shooter.getInstance();
-  Climber climber = Climber.getInstance();
-  XboxControllerMetalCow controller = new XboxControllerMetalCow(0);
-  RobotDashboard dashboard = RobotDashboard.getInstance();
+  DriveTrain drive;// = DriveTrain.getInstance();
+  Intake intake;// = Intake.getInstance();
+  Shooter shooter;// = Shooter.getInstance();
+  Climber climber;// = Climber.getInstance();
+  XboxControllerMetalCow controller;// = new XboxControllerMetalCow(0);
+  RobotDashboard dashboard;// = RobotDashboard.getInstance();
   MCRCommand mission;
 
   /**
@@ -50,18 +55,48 @@ public class Robot extends TimedRobot {
   public void autonomousPeriodic() {
     mission.run();
   }
+  I2C.Port port = I2C.Port.kOnboard;
+  ColorSensorV3 sensor = new ColorSensorV3(port);
+  ColorMatch color = new ColorMatch();
+
+  final Color kBlueTarget = ColorMatch.makeColor(0.143, 0.427, 0.429);
+  final Color kGreenTarget = ColorMatch.makeColor(0.197, 0.561, 0.240);
+  final Color kRedTarget = ColorMatch.makeColor(0.561, 0.232, 0.114);
+  final Color kYellowTarget = ColorMatch.makeColor(0.361, 0.524, 0.113);
 
   @Override
   public void teleopInit() {
+
+    color.addColorMatch(kBlueTarget);
+    color.addColorMatch(kGreenTarget);
+    color.addColorMatch(kRedTarget);
+    color.addColorMatch(kYellowTarget);
   }
 
   @Override
   public void teleopPeriodic() {
-    SmartDashboard.putNumber("Gyro", drive.getAngle());
-    intake.lowerIntake();
+    SmartDashboard.putNumber("red", sensor.getRed());
+    SmartDashboard.putNumber("green", sensor.getGreen());
+    SmartDashboard.putNumber("blue", sensor.getBlue()); 
+    SmartDashboard.putNumber("proximity", sensor.getProximity()); 
+    ColorMatchResult result = color.matchClosestColor(sensor.getColor());
+    SmartDashboard.putNumber("confidence", result.confidence); 
+    
+    if (result.color == kRedTarget) {
+      SmartDashboard.putString("color", "red");
+    }
+    else if (result.color == kGreenTarget) {
+      SmartDashboard.putString("color", "green");
+    }
+    else if (result.color == kBlueTarget) {
+      SmartDashboard.putString("color", "blue");
+    }
+    else if (result.color == kYellowTarget) {
+      SmartDashboard.putString("color", "yellow");
+    }
     // intake.retractIntake();
   }
-
+  
   @Override
   public void testInit() {
   }
