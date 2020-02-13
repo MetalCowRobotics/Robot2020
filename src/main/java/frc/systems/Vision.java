916221fc8f78e90b6cbe7740efbba2b539d5125d
metalcow-student <46615373+metalcow-student@users.noreply.github.com>
2020-02-12 19:57:34 -0600

    Update Vision.java

--- src/main/java/frc/systems/Vision.java
+++ src/main/java/frc/systems/Vision.java
@@ -3,6 +3,7 @@ package frc.systems;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.Joystick;
 import frc.commands.TurnDegrees;
 import frc.lib14.CommandPause;
 import frc.lib14.MCRCommand;
@@ -14,7 +15,7 @@ public class Vision {
     double focalHeight = 741.1590;
     double targetWidth = 3.3125;
     double targetHeight = 1.4167;
-    
+
     double distanceWidth = 0;
     double distanceHeight = 0;
     double distance = 0;
@@ -26,39 +27,69 @@ public class Vision {
     double targetTolerance = 3;
     double turnAdjustment = 60;
     double turnDelay = 180;
-    //MCRCommand turnCW =  new SequentialCommands(new TimedCommandSet(new TurnDegrees(turnAdjustment), 2),new CommandPause(.2));
-    //MCRCommand turnCCW = new SequentialCommands(new TimedCommandSet(new TurnDegrees(-turnAdjustment), 2),new CommandPause(.2));
-    TurnDegrees turnCW = new TurnDegrees(70);
-    TurnDegrees turnCCW = new TurnDegrees(-70);
 
+    double cameraFOV = 60;
+    double maxYaw = 25;
+    double yawDegrees = 0;
+    boolean targetMode = false;
+
+    // MCRCommand turnCW = new SequentialCommands(new TimedCommandSet(new
+    // TurnDegrees(turnAdjustment), 2),new CommandPause(.2));
+    // MCRCommand turnCCW = new SequentialCommands(new TimedCommandSet(new
+    // TurnDegrees(-turnAdjustment), 2),new CommandPause(.2));
+    //TurnDegrees turnCW = new TurnDegrees(70);
+    //TurnDegrees turnCCW = new TurnDegrees(-70);
 
     public NetworkTableEntry yaw;
     public NetworkTableEntry isDriverMode;
-    public NetworkTableEntry pitch;    
+    public NetworkTableEntry pitch;
     public NetworkTableEntry width;
     public NetworkTableEntry height;
-    public void robotInit() { 
-            NetworkTableInstance table = NetworkTableInstance.getDefault();
-            NetworkTable myCam = table.getTable("chameleon-vision").getSubTable("Mindstone");
-            yaw = myCam.getEntry("targetYaw");
-            pitch = myCam.getEntry("targetPitch");
-            isDriverMode = myCam.getEntry("driver_mode");
-            width = myCam.getEntry("targetFittedWidth");
-            height = myCam.getEntry("targetFittedHeight");
-            
+
+    public void visionInit() {
+        NetworkTableInstance table = NetworkTableInstance.getDefault();
+        NetworkTable myCam = table.getTable("chameleon-vision").getSubTable("Mindstone");
+        yaw = myCam.getEntry("targetYaw");
+        pitch = myCam.getEntry("targetPitch");
+        isDriverMode = myCam.getEntry("driver_mode");
+        width = myCam.getEntry("targetFittedWidth");
+        height = myCam.getEntry("targetFittedHeight");
+
     }
-    //Periodic function
-    public void teleopPeriodic() {
+
+    // Periodic function
+    public double getTargetDistance() {
         distanceWidth = focalWidth * targetWidth / width.getDouble(0.0);
         distanceHeight = focalHeight * targetHeight / height.getDouble(0.0);
         distance = (distanceHeight + distanceWidth) / 2;
         distanceSquared = distance * distance;
         horizontal = Math.sqrt(distanceSquared - heightSquared);
+        return horizontal;
+    }
+
+    public double getYawDegrees() {
+        yawDegrees = cameraFOV * 0.5 / maxYaw * yaw.getDouble(0.0);
+        return yawDegrees;
+    }
+
+    public void setTargetMode(boolean cameraMode) {
+        isDriverMode.setBoolean(!cameraMode);
+    }
+
+    public boolean getTargetMode() {
+        isDriverMode.getDouble(0.0);
+        targetMode = isDriverMode.getDouble(0.0) != 1.0;
+        return targetMode;
+    }
+
+/* 
+    public void teleopPeriodic() {
+        
         //System.out.println("hello world");
         //System.out.println("Yaw: " + yaw.getDouble(0.0) + "  Pitch: " + pitch.getDouble(0.0)+ "  targetFittedWidth " + width.getDouble(0.0));//trys to print yaw, if it doesnt exist it will print 0
         System.out.println("Distance: " + distance + "  distanceHeight: " + distanceHeight + "   distanceWidth" + distanceWidth + "   yaw" + yaw.getDouble(0.0));
         targetYaw = yaw.getDouble(0.0);
-        if (targetYaw > targetTolerance) { 
+       if (targetYaw > targetTolerance) { 
            //turn clockwise
            targetYaw = yaw.getDouble(0.0);
            turnCW.updateDegrees(targetYaw);
@@ -74,6 +105,6 @@ public class Vision {
           System.out.println("New Target Yaw: " + targetYaw);
           turnDelay=1;
           }   
-          turnDelay--;
-    }
+          turnDelay--; 
+    }*/
 }
