f6034e1566f1c67f27242dcbc880da98e4380a2a
metalcow-student <46615373+metalcow-student@users.noreply.github.com>
2020-02-22 11:31:32 -0600

    clean the robot class

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -10,17 +10,12 @@ package frc.robot;
 import edu.wpi.cscore.UsbCamera;
 import edu.wpi.first.cameraserver.CameraServer;
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.autonomous.NoAuto;
 import frc.autonomous.ShootAndGather;
 import frc.autonomous.ShootAndGo;
-import frc.commands.NewTurn;
 import frc.lib14.MCRCommand;
-import frc.lib14.XboxControllerMetalCow;
-import frc.robot.RobotMap.Magazine;
 import frc.systems.Climber;
 import frc.systems.DriveTrain;
-import frc.systems.Hood;
 import frc.systems.Intake;
 import frc.systems.MasterControls;
 import frc.systems.Shooter;
@@ -34,23 +29,17 @@ import frc.systems.Vision;
 
 public class Robot extends TimedRobot {
   // systems
-  DriveTrain driveTrain;// = DriveTrain.getInstance();
-  Intake intake;// = Intake.getInstance();
-  Shooter shooter;// = Shooter.getInstance();
-  Climber climber;// = Climber.getInstance();
+  DriveTrain driveTrain = DriveTrain.getInstance();
+  Intake intake = Intake.getInstance();
+  Shooter shooter = Shooter.getInstance();
+  Climber climber = Climber.getInstance();
   MasterControls controls = MasterControls.getInstance();
   RobotDashboard dashboard = RobotDashboard.getInstance();
   Vision vision = Vision.getInstance();
-  XboxControllerMetalCow controller = new XboxControllerMetalCow(0);
 
   // class variables
   MCRCommand mission;
 
-  // testing only
-  Magazine magazine;// = Magazine.getInstance();
-  // Turret turret = Turret.getInstance();
-  Hood hood = Hood.getInstance();
-  boolean firstTime = true;
 
   /**
    * This function is run when the robot is first started up and should be used
@@ -59,7 +48,7 @@ public class Robot extends TimedRobot {
 
   @Override
   public void robotInit() {
-    //driveTrain.calibrateGyro();
+    driveTrain.calibrateGyro();
     dashboard.pushAuto();
     dashboard.pushTurnPID();
   }
@@ -74,8 +63,6 @@ public class Robot extends TimedRobot {
     } else {
       mission = new NoAuto();
     }
-    // testing
-    mission = new NewTurn(90);
   }
 
   @Override
@@ -87,69 +74,40 @@ public class Robot extends TimedRobot {
   @Override
   public void teleopInit() {
     vision.visionInit();
-    hood.resetEncoder();
-    //shooter.setTargetSpeed(SmartDashboard.getNumber("Set Velocity", 1500));// needs velocity
   }
 
   @Override
   public void teleopPeriodic() {
-    //controls.changeMode();
-    //applyOperatorInputs();
-    // runSystemsStateMachine();
+    controls.changeMode();
+    applyOperatorInputs();
+    runSystemsStateMachine();
+  }
 
-    //check if operator wants to shoot
+  private void applyOperatorInputs() {
+    // check if operator wants to shoot
     if (controls.prepairToShoot()) {
       shooter.prepairToShoot();
     } else {
       shooter.stopShooter();
     }
-
-    //set shooting mode
-    if (controller.getYButton()) {
-      shooter.shootNow = true;
-    } else if (controller.getAButton()) {
-      shooter.shootNow = false;
-    }
-
-    //do on first time
-    if (firstTime) {
-      shooter.setupShooter();
-      firstTime = false;
-    }
-
-    //shoot ball
+    // shoot ball
     if (controls.shootNow()) {
       shooter.shootBall();
     } else if (controls.shootWhenReady()) {
       shooter.shootBallWhenReady();
     }
-
-    //run systems
-    shooter.run();
-    intake.run();
-
-    SmartDashboard.putNumber("distance", vision.getTargetDistance());
-    SmartDashboard.putNumber("yaw", vision.getYawDegrees());
-  }
-
-  private void applyOperatorInputs() {
     // intake
     if (controls.lowerIntake()) {
       intake.lowerIntake();
     } else if (controls.raiseIntake()) {
       intake.retractIntake();
     }
-    // shooter
-    if(controls.intakeOnOff()){
+    if (controls.intakeOnOff()) {
       intake.toggleIntakeState();
     }
-    //shooter
-    if (controls.shootWhenReady()) {
-     // shooter.shootBallWhenReady();
-    }
-    //climber
+    // climber
     climber.raiseClimber(controls.climbSpeed());
-  } 
+  }
 
   private void runSystemsStateMachine() {
     driveTrain.drive();
