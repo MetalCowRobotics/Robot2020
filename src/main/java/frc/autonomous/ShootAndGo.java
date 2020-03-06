35f10bb85422dd1ca0fb3d2d2402b93b9bf46ea1
metalcow-student <46615373+metalcow-student@users.noreply.github.com>
2020-03-05 22:25:00 -0600

    add turn and shoot

--- src/main/java/frc/autonomous/ShootAndGo.java
+++ src/main/java/frc/autonomous/ShootAndGo.java
@@ -1,10 +1,3 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
-
 package frc.autonomous;
 
 import frc.commands.DriveBackwardsStraight;
@@ -17,10 +10,8 @@ public class ShootAndGo implements MCRCommand {
     MCRCommand mission;
 
     public ShootAndGo() {
-        ShootBall shoot = new ShootBall();
-        MCRCommand commandSet = new SequentialCommands(shoot, shoot, shoot);
-        //  MCRCommand commandSet = new SequentialCommands(new ShootBall(), new ShootBall(), new ShootBall());
-         mission = new SequentialCommands(new TimedCommandSet(shoot, 8), new DriveBackwardsStraight(36, 4));
+        MCRCommand shoot = new ShootBall();
+        mission = new SequentialCommands(new TimedCommandSet(shoot, 8), new DriveBackwardsStraight(36, 4));
     }
 
     @Override
--- src/main/java/frc/autonomous/TurnShootAndGo.java
+++ src/main/java/frc/autonomous/TurnShootAndGo.java
@@ -1,18 +1,27 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
-
 package frc.autonomous;
 
-/**
- * Add your docs here.
- */
-public class TurnShootAndGo {
-    ShootBall shoot = new ShootBall();
-    MCRCommand commandSet = new SequentialCommands(shoot, shoot, shoot);
-    //  MCRCommand commandSet = new SequentialCommands(new ShootBall(), new ShootBall(), new ShootBall());
-     mission = new SequentialCommands(new TimedCommandSet(shoot, 8), new DriveBackwardsStraight(36, 4));
+import frc.commands.DriveBackwardsStraight;
+import frc.commands.ShootBall;
+import frc.commands.SpinUpDrum;
+import frc.commands.TurnTurret;
+import frc.lib14.MCRCommand;
+import frc.lib14.ParallelCommands;
+import frc.lib14.SequentialCommands;
+import frc.lib14.TimedCommandSet;
+
+public class TurnShootAndGo implements MCRCommand {
+    MCRCommand stepOne = new ParallelCommands(new SpinUpDrum(), new TurnTurret(-90));
+    MCRCommand stepTwo = new ShootBall();
+    MCRCommand mission = new SequentialCommands(stepOne, new TimedCommandSet(stepTwo, 8), new DriveBackwardsStraight(36, 4));
+
+    @Override
+    public void run() {
+        mission.run();
+    }
+
+    @Override
+    public boolean isFinished() {
+        return mission.isFinished();
+    }
+
 }
