722ff728b826d6e1d00d1c0a2a76109517e6caeb
metalcow-student <59151112+Quantum357@users.noreply.github.com>
2021-03-22 21:20:49 -0500

    tuning

--- src/main/java/frc/autonomous/BarrelRacing.java
+++ src/main/java/frc/autonomous/BarrelRacing.java
@@ -11,9 +11,9 @@ public class BarrelRacing implements MCRCommand {
 
     public BarrelRacing() {
         // MCRCommand firstLoop = new SequentialCommands(new TurnDegrees(-14.9), new DriveInches(1, 108.1), new CurvatureDrive("right", 360, 60, 0.7));
-        MCRCommand firstLoop = new SequentialCommands(new DriveInches(1, 108.1), new CurvatureDrive("right", 360, 48, 0.7));
+        MCRCommand firstLoop = new SequentialCommands(new DriveInches(1, 90), new CurvatureDrive("RIGHT", 361, 42, 0.8));
         // MCRCommand secondLoop = new SequentialCommands(new TurnDegrees(40.2), new DriveInches(1, 91.5), new CurvatureDrive("left", 360, 60, 0.7));
-        MCRCommand secondLoop = new SequentialCommands( new DriveInches(1, 91.5), new CurvatureDrive("left", 360, 60, 0.7));
+        MCRCommand secondLoop = new SequentialCommands( new DriveInches(1, 73), new CurvatureDrive("LEFT", 305, 42, 0.8));
         //MCRCommand thirdLoop = new SequentialCommands(new TurnDegrees(21.8), new DriveInches(1, 68.38), new CurvatureDrive("left", 226.6, 60, 0.7));
         // MCRCommand collect = new ParallelCommands(new IntakeDeployRun(), new AutoTarget(true), new DriveInches( 1, 144));
        // mission = new SequentialCommands(
@@ -23,8 +23,9 @@ public class BarrelRacing implements MCRCommand {
         // mission = new SequentialCommands(new CurvatureDrive("right", 90, 50, 0.8), new DriveInches(1, 108.1));
         // mission = new DriveInches(1, 91.5);
         // mission = new TurnDegrees(90);
-        mission = new SequentialCommands(
-               firstLoop, secondLoop);
+        mission = new SequentialCommands(firstLoop, secondLoop, new DriveInches(1, 70), new CurvatureDrive("LEFT", 228, 48, 0.7), new DriveInches(1, 275));
+        // mission = new CurvatureDrive("LEFT", 360, 60, 0.7);
+        // mission = secondLoop;
 
     }
     public void run() {
