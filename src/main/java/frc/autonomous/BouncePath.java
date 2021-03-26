08a9e80b582910e59894130681a1b26514877004
metalcow-student <59151112+Quantum357@users.noreply.github.com>
2021-03-26 18:08:01 -0500

    updates

--- src/main/java/frc/autonomous/BouncePath.java
+++ src/main/java/frc/autonomous/BouncePath.java
@@ -7,6 +7,7 @@ import frc.commands.TurnDegrees;
 import frc.commands.DriveInches;
 import frc.commands.DriveStraightInches;
 import frc.lib14.SequentialCommands;
+import frc.lib14.TimedCommandSet;
 
 
 public class BouncePath implements MCRCommand {
@@ -14,13 +15,14 @@ public class BouncePath implements MCRCommand {
 
 
     public BouncePath() {
-        MCRCommand firstPeak = new CurvatureDrive("LEFT", 109, 66, 0.8);
+        MCRCommand firstPeak = new CurvatureDrive("LEFT", 110, 66, 0.8);
         MCRCommand firstValley = new SequentialCommands(new TurnDegrees(153.435), new DriveInches(1, 22.361), new TurnDegrees(26.565), new DriveInches(1, 50), new TurnDegrees(-45), new DriveInches(1, 70.711));
         MCRCommand secondPeak = new SequentialCommands(new TurnDegrees(-45), new CurvatureDrive("left", 90, 30, 0.8), new DriveInches(1, 90), new TurnDegrees(180), new DriveInches(1, 90));
         MCRCommand secondValley = new SequentialCommands(new CurvatureDrive("left", 90, 30, 0.8), new DriveInches(1, 30), new CurvatureDrive("left", 90, 30, 0.8));
         MCRCommand thirdPeak = new SequentialCommands(new DriveInches(1, 90), new TurnDegrees(180), new CurvatureDrive("left", 90, 60, 0.8));
         mission = new SequentialCommands(firstPeak, firstValley, secondPeak, secondValley, thirdPeak);
-        mission = new SequentialCommands(firstPeak, new DriveBackwardsStraight(156), new TurnDegrees(80), new CurvatureDrive("LEFT", 42, 108, 0.8), new DriveStraightInches(54));
+        thirdPeak = new SequentialCommands(new TimedCommandSet(new TurnDegrees(90), 2), new CurvatureDrive("LEFT", 90, 108, 0.8) );
+        mission = new SequentialCommands(firstPeak, new DriveBackwardsStraight(144), new TimedCommandSet(new TurnDegrees(85), 2) , new CurvatureDrive("LEFT", 58, 110, 0.8), new DriveStraightInches(64), new DriveBackwardsStraight(120), thirdPeak);
        // mission = new CurvatureDrive("LEFT", 42, 108, 0.8);
         //new TurnDegrees(150), new CurvatureDrive("LEFT", 5, 144, 0.8)
     }
