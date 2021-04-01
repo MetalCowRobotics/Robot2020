3d44031ca9696d39fff0df87cc00ee6714449c96
Gokul Balaji <gokulbalaji05@gmail.com>
2021-04-01 18:16:14 -0500

    assigned sections of path to mission

--- src/main/java/frc/autonomous/GalacticSearch.java
+++ src/main/java/frc/autonomous/GalacticSearch.java
@@ -10,11 +10,12 @@ public class GalacticSearch implements MCRCommand {
     MCRCommand mission;
 
     public GalacticSearch() {
-        MCRCommand firstTwoBalls = new MCRCommand(new TurnDegrees(-20.79), new DriveInches(84.5), new TurnDegrees(110.79), new DriveInches(30));
-        MCRCommand secondTwoBalls = new MCRCommand(new TurnDegrees(-63.43), new DriveInches(67.08), new TurnDegrees(22.08), new DriveInches(42.43));
-        MCRCommand thirdTwoBalls = new MCRCommand(new TurnDegrees(-135), new DriveInches(120));
-        MCRCommand forthTwoBalls = new MCRCommand(new TurnDegrees(135), new DriveInches(42.43), new TurnDegrees(-45), new DriveInches(30));
-        MCRCommand fifthTwoBalls = new MCRCommand(new TurnDegrees(45), new DriveInches(84.85), new TurnDegrees(-45), new DriveInches(30));
+        MCRCommand firstTwoBalls = new SequentialCommands(new TurnDegrees(-20.79), new DriveInches(84.5), new TurnDegrees(110.79), new DriveInches(30));
+        MCRCommand secondTwoBalls = new SequentialCommands(new TurnDegrees(-63.43), new DriveInches(67.08), new TurnDegrees(22.08), new DriveInches(42.43));
+        MCRCommand thirdTwoBalls = new SequentialCommands(new TurnDegrees(-135), new DriveInches(120));
+        MCRCommand forthTwoBalls = new SequentialCommands(new TurnDegrees(135), new DriveInches(42.43), new TurnDegrees(-45), new DriveInches(30));
+        MCRCommand fifthTwoBalls = new SequentialCommands(new TurnDegrees(45), new DriveInches(84.85), new TurnDegrees(-45), new DriveInches(30));
+        mission = new SequentialCommands(firstTwoBalls, secondTwoBalls, thirdTwoBalls, forthTwoBalls, fifthTwoBalls);
     }
     public void run() {
 
